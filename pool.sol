c56088e9ecdbeee09b53b044a48843ed7f6c3bdb
Constantino <Okonemmanuelmma@gmail.com>
2023-05-03 18:24:20 +0100

    #feature swaprates function

--- pool.sol
+++ pool.sol
@@ -7,16 +7,15 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 interface pool{
     function deposit(address tokenAddr,uint amount) external;
     function withdrawal(address token, address to, uint256 amount) external;
-    function swap(address fromToken, address toToken,address to, uint256 amount) external;
+    function swap(address from, address to, uint256 amount) external;
 }
 contract Pool{
 
-    IERC20 public token;
-    mapping(address => mapping(address => uint)) balances;
+    mapping(address => mapping(address => uint)) public balances;
 
     function deposit(address tokenAddr, uint amount) external{
-        token = IERC20(tokenAddr);
-        if(token.balanceOf(msg.sender) >= amount) revert( "You don't have enough tokens to swap with");
+        IERC20 token = IERC20(tokenAddr);
+        require(token.balanceOf(msg.sender) >= amount, "You don't have enough tokens to swap with");
         if(token.allowance(msg.sender, address(this)) >= amount) token.transferFrom(msg.sender, address(this), amount);
         else {
             token.approve(address(this), 0);
@@ -27,10 +26,25 @@ contract Pool{
     }
 
     function withdrawal(address tokenAddr, uint amount) external{
-        token = IERC20(tokenAddr);
-        if(balances[msg.sender][tokenAddr] >= amount) revert("user does not have enough deposit for this withdrawal");
+        IERC20 token = IERC20(tokenAddr);
+    require(balances[msg.sender][tokenAddr] >= amount,"user does not have enough deposit for this withdrawal");
         
         token.transfer(msg.sender, amount);
         balances[msg.sender][tokenAddr] -= amount;
     }
+
+    function swapRates(address from, address to) view public returns gfs(uint256){
+        IERC20 fromToken = IERC20(from);
+        IERC20 toToken = IERC20(to);
+
+        require(fromToken.balanceOf(address(this)) != 0, "This token does not exist on this pool");
+        require(toToken.balanceOf(address(this)) != 0, "this token does not exist on this pool");
+
+        return fromToken.balanceOf(address(this))/toToken.balanceOf(address(this));
+    } 
+
+    // function swap(address from, address to, uint amount ) external{
+    //     IERC20 fromToken = IERC20(from);
+    //     IERC20 toToken = IERC20(to);
+    // }
 }
